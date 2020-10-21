673f46b4fe0bf57670386492f891d14a5edb1e93
Thamoray <abdellatifthamer@gmail.com>
2020-10-21 14:28:59 +0200

    Initial Author Routes

--- routes/authors.js
+++ routes/authors.js
@@ -4,8 +4,24 @@ const router = express.Router()
 const Author = require('../models/author')
 
 // All authors route 
-router.get('/',(req,res)=>{
-    res.render('authors/index')
+router.get('/',async (req,res)=>{
+   let searchOptions ={}
+   if(req.query.name != null && req.query.name !== '' ){
+       searchOptions.name= new RegExp (req.query.name, 'i')
+   }
+
+
+    try{
+      const authors = await Author.find(searchOptions)
+      res.render('authors/index', {
+          authors: authors,
+          searchOptions:req.query
+        })
+
+    }catch{
+        res.redirect('/')
+
+    }
 })
 
 // New authors route
@@ -13,22 +29,22 @@ router.get('/new',(req,res)=>{
     res.render('authors/new',{author: new Author()})
 })
 //Create author route
-router.post('/',(req,res)=>{
+router.post('/',async(req,res)=>{
     const author = new Author({
         name: req.body.name
     })
-    author.save((err, newAuthor)=>{
-        if(err){
-            res.render('authors/new',{
-                author: author,
-                errorMessage: 'Error creating Author'
-            })
-        }else{
-            //res.redirect(`authors/${newAuthor.id}`)
-            res.redirect(`authors`)
-        }
-       
-    })
+    try{
+        // await for async call from mongoose
+        const newAuthor = await author.save()
+       // res.redirect(`authors/${newAuthor.id}`)
+        res.redirect(`authors`)
+    }catch{
+        res.render('authors/new',{
+            author: author,
+            errorMessage: 'Error creating Author' })
+
+    }
+    
 
 })
 
