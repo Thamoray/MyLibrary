de56f41d8e61645bcf541f790b884cfb9828ed2c
Thamoray <abdellatifthamer@gmail.com>
2020-10-26 10:44:37 +0100

    upload file to the DB

--- models/book.js
+++ models/book.js
@@ -1,48 +1,45 @@
-const mongoose =require('mongoose')
+const mongoose = require('mongoose')
 
-const path = require('path')
-
-const coverImageBasePath = 'uploads/bookCovers'
-
-const bookSchema =new mongoose.Schema({
-  title:{
-      type:String,
-      required: true
-  },
-  description:{
-    type:String
-  
-  },
-  publishDate:{
-    type:Date,
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
     required: true
   },
-  pageCount:{
-    type:Number,
+  description: {
+    type: String
+  },
+  publishDate: {
+    type: Date,
     required: true
   },
-  createdAt:{
-    type:Date,
+  pageCount: {
+    type: Number,
+    required: true
+  },
+  createdAt: {
+    type: Date,
     required: true,
     default: Date.now
   },
-  coverImageName:{
-    type:String,
+  coverImage: {
+    type: Buffer,
     required: true
   },
-  author:{
-    type:mongoose.Schema.Types.ObjectId,
+  coverImageType: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
     required: true,
     ref: 'Author'
-  },
-
-})
-
-bookSchema.virtual('coverImagePath').get(function(){
-  if(this.coverImageName != null){
-    return path.join('/', coverImageBasePath, this.coverImageName)
   }
 })
 
-module.exports = mongoose.model('Book',bookSchema)
-module.exports.coverImageBasePath = coverImageBasePath
+bookSchema.virtual('coverImagePath').get(function() {
+  if (this.coverImage != null && this.coverImageType != null) {
+    return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage.toString('base64')}`
+  }
+})
+
+module.exports = mongoose.model('Book', bookSchema)
--- routes/books.js
+++ routes/books.js
@@ -1,21 +1,10 @@
-const express =require('express')
-const Book = require('../models/book')
+const express = require('express')
 const router = express.Router()
-const path = require('path')
-const fs = require('fs')
-const uploadPath = path.join('public', Book.coverImageBasePath)
-const imageMimeTypes = ['image/jpeg', 'image/png', 'images/gif']
+const Book = require('../models/book')
 const Author = require('../models/author')
-const multer = require('multer')
-const upload = multer({
-  dest: uploadPath,
-  fileFilter: (req, file, callback) => {
-    callback(null, imageMimeTypes.includes(file.mimetype))
-  }
-})
+const imageMimeTypes = ['image/jpeg', 'image/png', 'images/gif']
 
-
-// All Books route 
+// All Books Route
 router.get('/', async (req, res) => {
   let query = Book.find()
   if (req.query.title != null && req.query.title != '') {
@@ -42,35 +31,26 @@ router.get('/', async (req, res) => {
 router.get('/new', async (req, res) => {
   renderNewPage(res, new Book())
 })
-//Create book route
-router.post('/', upload.single('cover'), async (req, res) => {
-  const fileName = req.file != null ? req.file.filename : null
+
+// Create Book Route
+router.post('/', async (req, res) => {
   const book = new Book({
     title: req.body.title,
     author: req.body.author,
     publishDate: new Date(req.body.publishDate),
     pageCount: req.body.pageCount,
-    coverImageName: fileName,
     description: req.body.description
   })
+  saveCover(book, req.body.cover)
 
   try {
     const newBook = await book.save()
     // res.redirect(`books/${newBook.id}`)
     res.redirect(`books`)
   } catch {
-    if (book.coverImageName != null) {
-      removeBookCover(book.coverImageName)
-    }
     renderNewPage(res, book, true)
   }
 })
-
-function removeBookCover(fileName) {
-  fs.unlink(path.join(uploadPath, fileName), err => {
-    if (err) console.error(err)
-  })
-}
 
 async function renderNewPage(res, book, hasError = false) {
   try {
@@ -86,4 +66,13 @@ async function renderNewPage(res, book, hasError = false) {
   }
 }
 
-module.exports=router
+function saveCover(book, coverEncoded) {
+  if (coverEncoded == null) return
+  const cover = JSON.parse(coverEncoded)
+  if (cover != null && imageMimeTypes.includes(cover.type)) {
+    book.coverImage = new Buffer.from(cover.data, 'base64')
+    book.coverImageType = cover.type
+  }
+}
+
+module.exports = router
